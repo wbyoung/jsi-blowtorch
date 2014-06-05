3130e1b271532285404d92dad0f9534aac08d7eb
Whitney Young <wbyoung@fadingred.com>
2014-06-05 15:26:27 -0700

    Added use strict.

--- test/generator.js
+++ test/generator.js
@@ -1,10 +1,11 @@
+'use strict';
+
 var expect = require('chai').expect;
 var path = require('path');
 var fs = require('fs');
 var lib = require('../lib');
 var fsExtras = require('../lib/fs-extras');
 var temp = require('temp').track();
-
 
 describe('generator', function() {
   it.skip('generates sites', function(done) {
